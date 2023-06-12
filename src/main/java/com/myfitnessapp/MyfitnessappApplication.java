13366a10ab0b24dfd9b57b2458407f1a5c217c78
lukaszdolega <lukasz-dole@wp.pl>
2023-06-12 10:48:11 +0200

    first commit

---
+++ src/main/java/com/myfitnessapp/MyfitnessappApplication.java
@@ -0,0 +1,13 @@
+package com.myfitnessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfitnessappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfitnessappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myfitnessapp/MyfitnessappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfitnessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfitnessappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
